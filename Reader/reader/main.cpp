256967a767698c91ded1159f2e1f8024f41a073b
jmigual <j.marce.igual@gmail.com>
2015-06-01 12:43:17 +0200

    Afegit dades excel

--- Reader/reader/main.cpp
+++ Reader/reader/main.cpp
@@ -9,27 +9,53 @@ int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
     
-    if (argc <= 2) {
+    if (argc <= 3) {
         qWarning() << "Error, no output file loaded";
         return 0;
     }
     
-    QFile f(argv[1]);
-    f.open(QIODevice::ReadOnly);
-    QTextStream cin(&f);
+    QFile f1(argv[1]);
+    f1.open(QIODevice::ReadOnly);
+    QTextStream cin(&f1);
+    
+    QFile f2(argv[2]);
+    f2.open(QIODevice::WriteOnly);
+    QTextStream out1(&f2);
+    
+    QFile f3(argv[3]);
+    f3.open(QIODevice::WriteOnly);
+    QTextStream out2(&f3);
     
     QString dat0;
     QString dat1;
-    QString fi("$");
-    cin >> dat0;
-    while(dat0 != fi) {
-        cin >> dat1;
-        dat0 += dat1;
-        break;
-        
-        cin >> dat0;
-    }
-    qDebug() << dat0 << dat1;
     
-    return;
+    while(not cin.atEnd()) {
+        cin >> dat0;
+        cin >> dat1;
+        dat0 += " " + dat1;
+        
+        QDateTime d = QDateTime::fromString(dat0, "dd/MM/yyyy hh:mm:ss");
+        dat0 = d.toString("dd/MM/yyyy hh:mm:ss");
+        
+        int n;
+        cin >> n;        
+        uint nClasse = 0;
+        uint sClasse = 0;
+        
+        
+        for (int i = 0; i < n; ++i) {
+            cin >> dat1;
+            int temp;
+            cin >> temp;
+            int classe;
+            cin >> classe;
+            
+            classe ? sClasse += temp : nClasse += temp;
+        }
+        
+        out1 << dat0 << "\t" << nClasse << endl;
+        out2 << dat0 << "\t" << sClasse << endl;
+    }
+    
+    return 0;
 }
